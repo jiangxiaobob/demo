f25148ae3f488e3f86b1e71650eca3692c745efa
jiangxiaobob <133528566+jiangxiaobob@users.noreply.github.com>
2024-05-29 15:59:30 +0800

    Update

--- backup_zentao_nfs.sh
+++ backup_zentao_nfs.sh
@@ -3,15 +3,13 @@
 RED='\033[0;31m'
 NC='\033[0m'
 DATE=$(date +"%Y%m%d")
-####DB_USER="root"
-####DB_PASS="123456"
-####DB_PORT="13306"
 
 source_code_dir="/data/zentao/data/zentao/"
+source_data_dir="/data/zentao/data/mysql/data/"
 nfs_dir="/mnt/windows_share_data/zentao"
 backup_dir=$nfs_dir/$DATE
 backup_code_name="$backup_dir/code.tar.gz"
-#####backup_data_name="$backup_dir/db.sql"
+backup_data_name="$backup_dir/db.tar.gz"
 
 # 检查NFS共享目录和NFS数据目录是否存在，如果不存在，则停止脚本退出
 if [ ! -d "$nfs_dir" ]; then
@@ -33,23 +31,22 @@ if [ ! -d "$backup_dir" ] ; then
     fi
 fi
 
-# 备份数据库
-####mysqldump -h 127.0.0.1 -P $DB_PORT -u $DB_USER -p$DB_PASS --all-databases --single-transaction --routines --triggers --events > "$backup_data_name"
-###if [ $? -ne 0 ]; then
-###    echo -e "${RED}MySQL数据库备份失败${NC}"
-###else 
-###    echo "MySQL数据库备份成功"
-###fi
-
 # 备份目录文件
 tar -czvf "$backup_code_name" --exclude=tmp/backup -C "$source_code_dir" . 
 if [ $? -eq 0 ]; then
    echo "目录文件备份打包成功"
 else
-   echo -e "${RED}备份打包失败${NC}"
+   echo -e "${RED}目录备份打包失败${NC}"
+fi
+
+# 备份数据库文件
+tar -czvf "$backup_data_name"  -C "$source_data_dir" .
+if [ $? -eq 0 ]; then
+   echo "数据文件备份打包成功"
+else
+   echo -e "${RED}数据文件备份打包失败${NC}"
 fi
 
 # 删除超过7天的备份文件
-find $nfs_dir -type d -mtime +7 -exec rm -rf {} \;
-
+find $nfs_dir -maxdepth 1 -type d ! -name zentao -mtime +7 -exec rm -rf {} \;
 echo "执行结束"
